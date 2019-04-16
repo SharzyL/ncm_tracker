5d2eaaecc6aabcb86c5d757f34ed2792bea36985
Sharzy <sharzyluo@outlook.com>
2019-04-16 22:06:39 +0800

    a basic visualization

--- visualization.py
+++ visualization.py
@@ -1,25 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
+from database import Database
 
+plt.rcParams['font.sans-serif']=['Microsoft YaHei']
+database = Database()
+users = database.tracked()
 
-plt.rcdefaults()
-fig, ax = plt.subplots()
+if __name__ == '__main__':
+    for nickname in list(users.keys())[0:10]:
+        data = database.fetch_data(nickname)
+        times = list(data.keys())
+        times_offset = [(time - times[0]) / 3600 for time in times]
+        nums = list(data.values())
+        nums_offset = [num - nums[0] for num in nums]
+        plt.plot(times_offset, nums_offset, label=nickname)
 
-# Example data
-people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-y_pos = np.arange(len(people))
-performance = 3 + 10 * np.random.rand(len(people))
-error = np.random.rand(len(people))
-
-ax.barh(y_pos, performance, xerr=error, align='center',
-        color='green', ecolor='black')
-ax.set_yticks(y_pos)
-ax.set_yticklabels(people)
-ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Performance')
-ax.set_title('How fast do you want to go today?')
-
-plt.show()
+    plt.legend()
+    plt.show()
